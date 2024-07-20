3d44258be2ee55200d3a916fa6110c8f5d064e52
Derek Alves <derek.alves@somagrupo.com.br>
2024-07-20 18:07:09 -0300

    create build-logic add setting

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -7,3 +7,11 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,26 +1,13 @@
-pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    versionCatalogs{
-        create("libs"){
-            from(files("../gradle/libs.versions.toml"))
-        }
-    }
-}
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
     }
 }
 
